36ba8c7635dabd06012e61c4d4802cff74ab46f0
samir01 <samir.os.abdalla@gmail.com>
2023-11-14 09:19:45 -0800

    Fixed main page sizing and added navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import styles from './page.module.css'
-
+import Navbar from '@/components/Navbar/Navbar'
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <Navbar />
     </main>
   )
 }
