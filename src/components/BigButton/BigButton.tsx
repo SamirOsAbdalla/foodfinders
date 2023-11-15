a795dc2f9edb407ae924e9b546662b57259e58dd
samir01 <samir.os.abdalla@gmail.com>
2023-11-15 08:00:00 -0800

    Big button now fetches data from the yelp api through nextjs api routes

---
+++ src/components/BigButton/BigButton.tsx
@@ -0,0 +1,42 @@
+"use client"
+import "./BigButton.css"
+import { useState } from "react"
+
+export default function BigButton() {
+
+
+    const [userLocation, setUserLocation] = useState<{ latitude: Number, longitude: number } | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const geolocationSuccess = async (position: GeolocationPosition) => {
+        const coords = position.coords
+        setUserLocation({ latitude: coords.latitude, longitude: coords.longitude })
+        setLoading(true)
+
+        const yelpResponse = await fetch("/api/yelp", {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            }
+        })
+
+        let jsonResp = await yelpResponse.json()
+        setLoading(false)
+    }
+    const geolocationError = (error: GeolocationPositionError) => {
+        console.log("Error: Could not fetch user coordinates")
+    }
+    const handleBigButtonClick = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError)
+        }
+    }
+    return (
+        <div className={`bigbutton__wrapper ${loading == false && "bigbutton__loading"}`}>
+            <button onClick={handleBigButtonClick} className="bigbutton">
+                FIND FOOD
+            </button>
+        </div>
+    )
+}
