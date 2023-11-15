337f84a0627c86a2afa491a8564a4f4e2a25d4f8
samir01 <samir.os.abdalla@gmail.com>
2023-11-15 07:58:52 -0800

    Created main page to hold the button, button result, etc. Not the best name but it will do for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import styles from './page.module.css'
 import Navbar from '@/components/Navbar/Navbar'
+import MainPage from '@/components/MainPage/MainPage'
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
+      <MainPage />
     </main>
   )
 }
---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import BigButton from '@/components/BigButton/BigButton'
+import "./MainPage.css"
+
+export default function MainPage() {
+    return (
+        <section className="mainpage__wrapper">
+            <BigButton />
+        </section>
+    )
+}
