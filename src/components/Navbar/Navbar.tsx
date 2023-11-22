7889e740fb92481139f6407ce4b37a3a499dc593
samir01 <samir.os.abdalla@gmail.com>
2023-11-22 13:26:45 -0800

    finished styling/logic for navbar hamburger

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,6 +11,7 @@ export type DropdownStatus = "open" | "closed"
 export default function Navbar() {
     const [accountDropdownStatus, setAccountDropdownStatus] = useState<DropdownStatus>("closed")
     const [filterDropdownStatus, setFilterDropdownStatus] = useState<DropdownStatus>("closed")
+    const [hamburgerStatus, setHamburgerStatus] = useState<DropdownStatus>("closed")
 
     //could probably figure out a way to combine these two functions
     //but it's fine for now seeing as they are quite small
@@ -22,6 +23,7 @@ export default function Navbar() {
         }
         setAccountDropdownStatus("open")
     }
+
     const toggleFilterDropdownStatus = () => {
         setAccountDropdownStatus("closed")
         if (filterDropdownStatus == "open") {
@@ -29,6 +31,7 @@ export default function Navbar() {
             return;
         }
         setFilterDropdownStatus("open")
+        setHamburgerStatus("closed")
     }
     const accountDropdownRef = useRef<HTMLDivElement>(null)
     const navbarDropdownRef = useRef<HTMLDivElement>(null)
@@ -36,16 +39,31 @@ export default function Navbar() {
     useEffect(() => {
         window.onclick = (event) => {
             let htmlTarget = event.target as HTMLElement
+            console.log(htmlTarget)
             if (!accountDropdownRef.current?.contains(htmlTarget)) {
                 setAccountDropdownStatus("closed")
             }
+
+
+            if (!navbarDropdownRef.current?.contains(htmlTarget) && !(htmlTarget.classList.contains("navbar__hamburger__container") || htmlTarget.classList.contains("navbar__hamburger"))) {
+                setHamburgerStatus("closed")
+            }
         }
     }, [])
+
+    const handleHamburgerClick = (e: React.MouseEvent<HTMLDivElement>) => {
+
+        hamburgerStatus == "closed" ? setHamburgerStatus("open") : setHamburgerStatus("closed")
+
+    }
+
     return (
         <nav className="navbar d-flex justify-content-center p-3 bg-white position-fixed">
-            <div className="navbar__container d-flex justify-content-between align-items-center">
-                <div className="navbar__hamburger">
+            <div className={`navbar__container ${hamburgerStatus == "open" && "navbar__container__open"} d-flex justify-content-between align-items-center`}>
+                <div onClick={handleHamburgerClick} className="navbar__hamburger__container">
+                    <div className="navbar__hamburger">
 
+                    </div>
                 </div>
                 <div className="d-flex navbar__left align-items-center gap-3">
                     <div className="d-flex align-items-center justify-content-center navbar__brand navbar-brand m-0 h1">SpeedEats</div>
@@ -74,18 +92,21 @@ export default function Navbar() {
                 </div>
                 {filterDropdownStatus == "open" && <FilterDropdown setFilterDropdownStatus={setFilterDropdownStatus} />}
             </div>
-            <div className="navbar__dropdown w-100 d-flex pt-3 align-items-start flex-column gap-4" ref={navbarDropdownRef}>
-                <Link className="navbar__dropdown__text text-decoration-none" href="/favorites">
-                    Favorites
-                </Link>
-                <Link className="navbar__dropdown__text text-decoration-none" href="/about">
-                    About
-                </Link>
-                <Link href="/login" className="text-decoration-none logout__button d-flex align-items-center justify-content-center">
-                    Logout
-                    <HiLogout />
-                </Link>
-            </div>
+            {hamburgerStatus == "open" &&
+                <div className="navbar__dropdown justify-content-start align-items-start flex-column gap-4" ref={navbarDropdownRef}>
+                    <Link className="navbar__dropdown__text text-decoration-none" href="/favorites">
+                        Favorites
+                    </Link>
+                    <Link className="navbar__dropdown__text text-decoration-none" href="/about">
+                        About
+                    </Link>
+                    <Link href="/login" className="text-decoration-none logout__button d-flex align-items-center justify-content-center">
+                        Logout
+                        <HiLogout />
+                    </Link>
+                </div>
+            }
+
         </nav>
     )
 }
