c1133694d76c2e4c5c4328f734b417d1f73cab99
samir01 <samir.os.abdalla@gmail.com>
2023-11-21 17:35:41 -0800

    Refactoring routes page to return api response from restaurant api wrapper

--- src/app/api/getNearbyRestaurant/route.ts
+++ src/app/api/getNearbyRestaurant/route.ts
@@ -35,4 +35,6 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const latitude = searchParams.get("latitude")
     const longitude = searchParams.get("longitude")
+
+    return Response.json(null)
 }
