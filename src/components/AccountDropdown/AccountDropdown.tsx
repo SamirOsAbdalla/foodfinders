578a03da5e471fd19c2008e91eba41b829f1488c
samir01 <samir.os.abdalla@gmail.com>
2023-11-21 17:36:59 -0800

    Restyled account dropdown and component now closes when user clicks outside

--- src/components/AccountDropdown/AccountDropdown.tsx
+++ src/components/AccountDropdown/AccountDropdown.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { SetStateAction, Dispatch } from 'react'
 import Link from 'next/link'
 import "./AccountDropdown.css"
-export default function AccountDropdown() {
+import { HiLogout } from "react-icons/hi";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { useRef, useEffect } from "react"
+import { DropdownStatus } from '../Navbar/Navbar';
+
+interface Props {
+    setAccountDropdownStatus: Dispatch<SetStateAction<DropdownStatus>>
+}
+export default function AccountDropdown({ setAccountDropdownStatus }: Props) {
+
     return (
-        <div className="accountdropdown__wrapper position-absolute d-flex flex-column p-3 gap-3">
-            <Link href="/saved" className="text-decoration-none accountdropdown__link">
-                Saved
+        <div className="accountdropdown__wrapper position-absolute d-flex flex-column py-2 gap-2">
+            <Link href="/saved" className="d-flex align-items-center justify-content-between gap-3 w-100 text-decoration-none accountdropdown__link">
+                Favorites
+                <MdOutlineFavoriteBorder />
             </Link>
-            <Link href="/login" className="text-decoration-none accountdropdown__link">
+            <Link href="/login" className="d-flex align-items-center justify-content-between gap-3 text-decoration-none accountdropdown__link">
                 {/*Get Session*/}
                 Logout
+                <HiLogout />
             </Link>
         </div>
     )
