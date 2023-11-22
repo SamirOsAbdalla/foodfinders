0b5736dfd4fcc071fd15c8f7cba583fbba48302f
samir01 <samir.os.abdalla@gmail.com>
2023-11-21 17:33:56 -0800

    Changed data test id and element type

--- __tests__/Navbar.test.tsx
+++ __tests__/Navbar.test.tsx
@@ -5,9 +5,9 @@ import { ReduxProvider } from "@/redux/provider"
 describe("Navbar", () => {
     it("should render the filter icon", () => {
         render(<Navbar />)
-        const filterIcon = screen.getByTestId("navbar__filter__icon")
+        const filterButton = screen.getByTestId("navbar__filter__button")
 
-        expect(filterIcon).toBeInTheDocument()
+        expect(filterButton).toBeInTheDocument()
     })
 
     it("should render the filter dropdown", () => {
@@ -16,8 +16,8 @@ describe("Navbar", () => {
                 <Navbar />
             </ReduxProvider>
         )
-        const filterIcon = screen.getByTestId("navbar__filter__icon")
-        fireEvent.click(filterIcon)
+        const filterButton = screen.getByTestId("navbar__filter__button")
+        fireEvent.click(filterButton)
         const filterdropdown = screen.getByTestId("filterdropdown")
         expect(filterdropdown).toBeInTheDocument()
     })
