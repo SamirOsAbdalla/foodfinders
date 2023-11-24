9c2a4a4f1ebc2195c242274ca1535467db899d28
samir01 <samir.os.abdalla@gmail.com>
2023-11-23 20:21:35 -0800

    Made the two modals wrap around the big button

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,16 @@
 import BigButton from '@/components/BigButton/BigButton'
 import "./MainPage.css"
+import HowToUseModal from '../HowToUseModal/HowToUseModal'
+import RestaurantHistory from '../RestaurantHistory/RestaurantHistory'
 
 export default function MainPage() {
     return (
+
         <section className="mainpage__wrapper">
+            <div className="mainpage__modals position-absolute d-flex justify-content-around w-100">
+                <HowToUseModal />
+                <RestaurantHistory />
+            </div>
             <BigButton />
         </section>
     )
