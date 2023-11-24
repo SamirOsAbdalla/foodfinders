fbb8b0c683a173e5a775368911381a68efa70aed
samir01 <samir.os.abdalla@gmail.com>
2023-11-23 20:19:28 -0800

    refactor api route to adhere to restaurant api wrapper interface

--- src/app/api/getNearbyRestaurant/route.ts
+++ src/app/api/getNearbyRestaurant/route.ts
@@ -1,40 +1,40 @@
 import { NextRequest, NextResponse } from "next/server"
+import { AcceptedFoodFilters, PriceRanges } from "restaurants-api-wrapper"
+const getANearbyRestaurant = require("restaurants-api-wrapper")
 
 
-
-// const baseYelpURL = `https://api.yelp.com/v3/businesses/search?sort_by=best_match`
-// export async function GET(request: NextRequest) {
-//     const searchParams = request.nextUrl.searchParams
-//     const latitude = searchParams.get("latitude")
-//     const longitude = searchParams.get("longitude")
-//     const apiOptions = {
-//         method: 'GET',
-//         headers: {
-//             "Accept": 'application/json',
-//             "Authorization": `Bearer ${process.env.NEXT_PUBLIC_YELP_API_KEY}`
-//         }
-//     }
-//     const taOptions = {
-//         method: "GET",
-//         headers: {
-//             "Accept": "application/json",
-//             "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TA_API_KEY}`
-//         }
-//     }
-//     if (latitude && longitude) {
-//         const fetchURL = baseYelpURL + `&latitude=${latitude}&longitude=${longitude}&limit=20`
-//         const response = await fetch(fetchURL, apiOptions)
-//         const response1 = await fetch(`https://api.content.tripadvisor.com/api/v1/location/nearby_search?key=${process.env.NEXT_PUBLIC_TA_API_KEY}&latLong=${latitude},${longitude}`, taOptions)
-//         console.log(await response1.json())
-//         return Response.json(await response.json())
-//     }
-
-// }
 
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const latitude = searchParams.get("latitude")
     const longitude = searchParams.get("longitude")
+    const cuisineString = searchParams.get("cuisineString")
+    const pricesString = searchParams.get("pricesString")
+
+    let foodTypesArray: any[] = []
+    let pricesArray: any[] = []
+
+    if (cuisineString) {
+        foodTypesArray = cuisineString.split(",")
+    }
+    if (pricesString) {
+        pricesArray = pricesString.split(",")
+    }
+
+    const apiKeyBundler = {
+        yelpKey: process.env.NEXT_PUBLIC_YELP_API_KEY,
+        tripAdvisorKey: process.env.NEXT_PUBLIC_TA_API_KEY,
+        tomTomKey: process.env.NEXT_PUBLIC_TOMTOM_API_KEY
+    }
+    const coordinates = { latitude, longitude }
+
+    const filtersObject = {
+        prices: pricesArray,
+        foodTypes: foodTypesArray
+    }
+
+    const response = await getANearbyRestaurant(coordinates, apiKeyBundler, filtersObject)
+    console.log("RESPONSE", response)
 
     return Response.json(null)
 }
