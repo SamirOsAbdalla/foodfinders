8527dd1887bd9cd12a0db0b0bdf8bbd9d22eda8f
samir01 <samir.os.abdalla@gmail.com>
2023-11-25 18:34:24 -0800

    Added cdn for yelp images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["s3-media2.fl.yelpcdn.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "s3-media2.fl.yelpcdn.com",
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
