f3c996a28e75633d2d3cb73bc9f5d9a12b2eb2c2
samir01 <samir.os.abdalla@gmail.com>
2023-11-26 12:03:15 -0800

    added more domain names

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["s3-media2.fl.yelpcdn.com"],
+        domains: [
+            "s3-media0.fl.yelpcdn.com",
+            "s3-media1.fl.yelpcdn.com",
+            "s3-media2.fl.yelpcdn.com",
+            "s3-media3.fl.yelpcdn.com",
+            "s3-media4.fl.yelpcdn.com",
+            "s3-media5.fl.yelpcdn.com"
+        ],
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "s3-media2.fl.yelpcdn.com",
+                hostname: "fl.yelpcdn.com",
             },
         ]
     },
