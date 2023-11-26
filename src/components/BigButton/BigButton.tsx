976c5b95fb893cf5c4c677e67772260e0ceb78d8
samir01 <samir.os.abdalla@gmail.com>
2023-11-25 18:37:04 -0800

    added different sizes for the button for later use inside of restaurant display

--- src/components/BigButton/BigButton.tsx
+++ src/components/BigButton/BigButton.tsx
@@ -5,16 +5,19 @@ import { IoFastFoodOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 
-export default function BigButton() {
+interface Props {
+    buttonSize: "small" | "regular"
+}
+export default function BigButton({ buttonSize }: Props) {
 
     const [userLocation, setUserLocation] = useState<{ latitude: Number, longitude: number } | null>(null)
     const [loading, setLoading] = useState<boolean>(false)
     const reduxFilterState = useSelector((state: RootState) => state.filterReducer.value)
 
     const geolocationSuccess = async (position: GeolocationPosition) => {
+        setLoading(true)
         const coords = position.coords
         setUserLocation({ latitude: coords.latitude, longitude: coords.longitude })
-        setLoading(true)
 
         const cuisineString = reduxFilterState.cuisine.join(",")
         const pricesString = reduxFilterState.prices.join(",")
@@ -30,12 +33,12 @@ export default function BigButton() {
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
-            },
-
+            }
         })
 
         const restaurantJSON = await restaurant.json()
 
+        setLoading(false)
     }
 
     const geolocationError = (error: GeolocationPositionError) => {
@@ -47,10 +50,10 @@ export default function BigButton() {
         }
     }
     return (
-        <div className={`bigbutton__wrapper ${loading == false && "bigbutton__loading"}`}>
-            <div className="bigbutton__container d-flex align-items-center justify-content-center">
-                <button onClick={handleBigButtonClick} className="bigbutton">
-                    <IoFastFoodOutline className="fastfood__icon" />
+        <div className={`bigbutton__wrapper`}>
+            <div className={`bigbutton__container__${buttonSize} ${loading == true && "bigbutton__container__loading"} d-flex align-items-center justify-content-center`}>
+                <button onClick={handleBigButtonClick} className={`bigbutton__${buttonSize} ${loading == true && "bigbutton__loading"}`}>
+                    <IoFastFoodOutline className={`fastfood__icon__${buttonSize}`} />
                 </button>
             </div>
 
