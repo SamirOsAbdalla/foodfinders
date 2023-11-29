c0d7a1e7fbbf593ad40748144a7e9b0f1a504656
samir01 <samir.os.abdalla@gmail.com>
2023-11-29 10:07:18 -0800

    added more CDN domain names

--- next.config.js
+++ next.config.js
@@ -7,12 +7,17 @@ const nextConfig = {
             "s3-media2.fl.yelpcdn.com",
             "s3-media3.fl.yelpcdn.com",
             "s3-media4.fl.yelpcdn.com",
-            "s3-media5.fl.yelpcdn.com"
+            "s3-media5.fl.yelpcdn.com",
+            "media-cdn.tripadvisor.com"
         ],
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "fl.yelpcdn.com",
+            },
+            {
+                protocol: "https",
+                hostname: "media-cdn.tripadvisor.com",
             },
         ]
     },
