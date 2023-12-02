24f3754bfbf2969dab73d95f7597c8f8a4a46182
samir01 <samir.os.abdalla@gmail.com>
2023-12-01 17:22:02 -0800

    added sizing for smaller button

--- src/components/BigButton/BigButton.tsx
+++ src/components/BigButton/BigButton.tsx
@@ -2,17 +2,19 @@
 import "./BigButton.css"
 import { useState } from "react"
 import { IoFastFoodOutline } from "react-icons/io5";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
-
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import { setCurrentRestaurant } from "@/redux/slices/currentRestaurant-slice";
+import { setRestaurantHistory } from "@/redux/slices/restaurantHistory-slice";
 interface Props {
     buttonSize: "small" | "regular"
 }
 export default function BigButton({ buttonSize }: Props) {
 
+    const reduxFilterState = useSelector((state: RootState) => state.filterReducer.value)
+    const dispatch = useDispatch<AppDispatch>()
     const [userLocation, setUserLocation] = useState<{ latitude: Number, longitude: number } | null>(null)
     const [loading, setLoading] = useState<boolean>(false)
-    const reduxFilterState = useSelector((state: RootState) => state.filterReducer.value)
 
     const geolocationSuccess = async (position: GeolocationPosition) => {
         setLoading(true)
@@ -21,8 +23,6 @@ export default function BigButton({ buttonSize }: Props) {
 
         const cuisineString = reduxFilterState.cuisine.join(",")
         const pricesString = reduxFilterState.prices.join(",")
-
-
         const fetchUrl = `/api/getNearbyRestaurant?` +
             `latitude=${coords.latitude}&longitude=${coords.longitude}` +
             `${cuisineString ? ("&cuisineString=" + cuisineString) : ""}` +
@@ -37,7 +37,11 @@ export default function BigButton({ buttonSize }: Props) {
         })
 
         const restaurantJSON = await restaurant.json()
-
+        if ("errorMessage" in restaurantJSON) {
+            //handle error
+        }
+        dispatch(setCurrentRestaurant(restaurantJSON))
+        dispatch(setRestaurantHistory(restaurantJSON))
         setLoading(false)
     }
 
@@ -45,18 +49,18 @@ export default function BigButton({ buttonSize }: Props) {
         console.log("Error: Could not fetch user coordinates")
     }
     const handleBigButtonClick = () => {
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError)
         }
     }
     return (
-        <div className={`bigbutton__wrapper`}>
-            <div className={`bigbutton__container__${buttonSize} ${loading == true && "bigbutton__container__loading"} d-flex align-items-center justify-content-center`}>
-                <button onClick={handleBigButtonClick} className={`bigbutton__${buttonSize} ${loading == true && "bigbutton__loading"}`}>
-                    <IoFastFoodOutline className={`fastfood__icon__${buttonSize}`} />
+        <div className={`bigbutton__wrapper bigbutton__wrapper__${buttonSize}`}>
+            <div onClick={handleBigButtonClick} className={`bigbutton__container bigbutton__container__${buttonSize} ${loading == true && "bigbutton__container__loading"} d-flex align-items-center justify-content-center`}>
+                <button className={`bigbutton bigbutton__${buttonSize} ${loading == true && "bigbutton__loading"}`}>
+                    <IoFastFoodOutline className={`fastfood__icon fastfood__icon__${buttonSize}`} />
                 </button>
             </div>
-
         </div>
     )
 }
