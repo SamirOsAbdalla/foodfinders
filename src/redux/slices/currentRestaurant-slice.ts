3cd9a2a71df0532932fdeddbaa8b1902dd5ca4f4
samir01 <samir.os.abdalla@gmail.com>
2023-12-01 17:20:36 -0800

    created slice to house current restaurant being displayed

---
+++ src/redux/slices/currentRestaurant-slice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TripAdvisorRestaurant, YelpRestaurant } from "@/util/restaurantTypes";
+type CurrentRestaurantState = {
+    currentRestaurant?: TripAdvisorRestaurant | YelpRestaurant
+}
+
+type InitialState = {
+    value: CurrentRestaurantState
+}
+const initialState = {
+    value: {
+        currentRestaurant: undefined
+    }
+} as InitialState
+
+export const currentRestaurant = createSlice({
+    name: "currentRestaurant",
+    initialState,
+    reducers: {
+        setCurrentRestaurant: (state, action: PayloadAction<CurrentRestaurantState>) => {
+            return ({
+                value: action.payload
+            })
+        }
+    }
+})
+
+export const { setCurrentRestaurant } = currentRestaurant.actions
+export default currentRestaurant.reducer
