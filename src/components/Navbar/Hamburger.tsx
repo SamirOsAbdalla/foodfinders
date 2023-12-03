aaf71f303706e29ab3abe55530ffc12c353b2f20
samir01 <samir.os.abdalla@gmail.com>
2023-12-02 16:09:09 -0800

    refactor: moved hamburger logic into seperate component

---
+++ src/components/Navbar/Hamburger.tsx
@@ -0,0 +1,24 @@
+import { DropdownStatus } from "./Navbar"
+import { Dispatch, SetStateAction, useState } from "react"
+
+interface Props {
+    hamburgerStatus: DropdownStatus,
+    setHamburgerStatus: Dispatch<SetStateAction<DropdownStatus>>
+}
+export default function Hamburger({
+    hamburgerStatus,
+    setHamburgerStatus
+}: Props) {
+
+    const handleHamburgerClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        hamburgerStatus == "closed" ? setHamburgerStatus("open") : setHamburgerStatus("closed")
+    }
+
+    return (
+        <div onClick={handleHamburgerClick} className="navbar__hamburger--container">
+            <div className="navbar__hamburger">
+
+            </div>
+        </div>
+    )
+}
