c9ed1b56533a9f9da2a96a156e1da45314f35706
samir01 <samir.os.abdalla@gmail.com>
2023-12-03 13:43:55 -0800

    refactor: changed name of HowToUseModal to Instructions

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -1,30 +1,33 @@
 "use client"
-import React from 'react'
-import "./HowToUseModal.css"
-import { FaCaretDown, FaCaretUp } from "react-icons/fa";
+import "./Instructions.css"
+import {
+    FaCaretDown,
+    FaCaretUp
+} from "react-icons/fa";
 import { useState } from 'react';
 
-export default function HowToUseModal() {
-    const [caretStatus, setCaretStatus] = useState<"up" | "down">("down")
+type CaretStatus = "up" | "down"
+export default function Instructions() {
+    const [caretStatus, setCaretStatus] = useState<CaretStatus>("down")
 
-    const handleCaretStatus = () => {
+    const toggleCaretStatus = () => {
         caretStatus == "down" ? setCaretStatus("up") : setCaretStatus("down")
     }
 
     return (
         <div className="htu__wrapper d-flex flex-column">
             <div className="htu__heading d-flex justify-content-center align-items-center">
-                <div className="htu__heading__container d-flex justify-content-between align-items-center">
+                <div className="htu__heading--container d-flex justify-content-between align-items-center">
                     Instructions
                     <div>
                         {caretStatus == "down" ?
-                            <FaCaretDown onClick={handleCaretStatus} className="htu__caret" /> :
-                            <FaCaretUp onClick={handleCaretStatus} className="htu__caret" />
+                            <FaCaretDown onClick={toggleCaretStatus} className="htu__caret" /> :
+                            <FaCaretUp onClick={toggleCaretStatus} className="htu__caret" />
                         }
                     </div>
                 </div>
             </div>
-            <div className={`htu__dropdown ${caretStatus == "up" && "htu__dropdown__show"} flex-column justify-content-start gap-4 pt-2`}>
+            <div className={`htu__dropdown pt-2 ${caretStatus == "up" && "htu__dropdown--show"} d-flex flex-column justify-content-start gap-4`}>
                 <span>1. Click on the button</span>
                 <span>2. Get your personalized restaurant</span>
                 <span>3. Call, favorite or go to the restaurant</span>
