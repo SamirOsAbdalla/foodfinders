2d917ea079cde943729d5a18b6216d3b4116c342
samir01 <samir.os.abdalla@gmail.com>
2023-12-02 16:10:27 -0800

    refactor: moved navbar links into seperate component

---
+++ src/components/Navbar/NavbarLink.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+interface Props {
+    text: string,
+    linkhref: string
+}
+export default function NavbarLink({
+    text,
+    linkhref
+}: Props) {
+    return (
+        <Link className="navbar__link text-decoration-none" href={`${linkhref}`}>
+            {text}
+        </Link>
+    )
+}
