1b3ce17152dcd39a972bb9ac27c491b0bc59353d
samir01 <samir.os.abdalla@gmail.com>
2023-12-05 11:01:13 -0800

    fix: solved issue where FilterDropdown was closing on a non-wrapper click

--- src/components/FilterDropdown/FilterDropdown.tsx
+++ src/components/FilterDropdown/FilterDropdown.tsx
@@ -22,8 +22,17 @@ export default function FilterDropdown({
         return (<></>)
     }
 
+    const wrapperClassName = "filter-dropdown__wrapper"
+    const handleWrapperClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        let target = e.target as HTMLElement
+        if (!target.classList.contains(wrapperClassName)) {
+            return
+        }
+        closeForm()
+    }
+
     return (
-        <div className="filter-dropdown__wrapper position-fixed top-0 start-0 end-0 bottom-0">
+        <div onClick={(e) => handleWrapperClick(e)} className={`${wrapperClassName} position-fixed top-0 start-0 end-0 bottom-0`}>
             <FilterForm
                 closeForm={closeForm}
             />
