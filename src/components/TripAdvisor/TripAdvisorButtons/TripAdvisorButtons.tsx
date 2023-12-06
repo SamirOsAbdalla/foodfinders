fb49383517262886d97cccbd464e3113ad95e0e2
samir01 <samir.os.abdalla@gmail.com>
2023-12-06 11:58:02 -0800

    refactor: moved location gathering logic into seperate hook

--- src/components/TripAdvisor/TripAdvisorButtons/TripAdvisorButtons.tsx
+++ src/components/TripAdvisor/TripAdvisorButtons/TripAdvisorButtons.tsx
@@ -2,7 +2,7 @@ import "./TripAdvisorButtons.css"
 import { IoCallOutline } from "react-icons/io5"
 import { FaMapMarkedAlt } from "react-icons/fa"
 import Link from "next/link"
-
+import { useLocation } from "@/components/Yelp/YelpUtilButtons/YelpUtilButtons"
 
 // Admittedly this can be a combined component along with the Yelp buttons
 // but making a generic component is a task for a later time
@@ -14,6 +14,12 @@ export default function TripAdvisorButtons({
     phoneNumber,
     address
 }: Props) {
+
+    const {
+        latitude,
+        longitude
+    } = useLocation()
+
     return (
         <div className="d-flex justify-content-start gap-2">
             {phoneNumber &&
@@ -23,7 +29,7 @@ export default function TripAdvisorButtons({
                 </Link>
             }
             {address &&
-                <Link href="" className="text-decoration-none tripadvisor__button tripadvisor__navigate">
+                <Link href={`http://maps.google.com/maps?saddr=${latitude},${longitude}&daddr=${address}`} className="text-decoration-none tripadvisor__button tripadvisor__navigate">
                     <FaMapMarkedAlt />
                     Navigate
                 </Link>
--- src/components/Yelp/YelpUtilButtons/YelpUtilButtons.tsx
+++ src/components/Yelp/YelpUtilButtons/YelpUtilButtons.tsx
@@ -2,16 +2,43 @@ import "./YelpUtilButtons.css"
 import Link from "next/link"
 import { FaMapMarkedAlt } from "react-icons/fa"
 import { IoCallOutline } from "react-icons/io5"
-
+import { useState } from "react"
 interface Props {
     phoneNumber?: string,
     address?: string
 }
+
+export const useLocation = () => {
+
+    let [latitude, setLatitude] = useState<number>(0)
+    let [longitude, setLongitude] = useState<number>(0)
+
+    const geolocationSuccess = (position: GeolocationPosition) => {
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+    }
+
+    const geolocationError = () => {
+
+    }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError)
+    }
+    return { latitude, longitude }
+}
+
 export default function YelpUtilButtons(
     {
         phoneNumber,
         address
     }: Props) {
+
+
+    let {
+        latitude,
+        longitude
+    } = useLocation()
+
     return (
         <div className="yelp-util-buttons__wrapper d-flex justify-content-start align-items-center gap-2">
             {phoneNumber &&
@@ -21,10 +48,10 @@ export default function YelpUtilButtons(
                 </Link>
             }
             {address &&
-                <button className="yelp-util__button">
+                <Link href={`http://maps.google.com/maps?saddr=${latitude},${longitude}&daddr=${address}`} target="_blank" className="text-decoration-none yelp-util__button d-flex justify-content-center align-items-center">
                     <FaMapMarkedAlt />
                     Navigate
-                </button>
+                </Link>
             }
         </div>
     )
