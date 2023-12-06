4c25b45568a4bc42a63666ec40068f1c73c0a26e
samir01 <samir.os.abdalla@gmail.com>
2023-12-06 11:52:30 -0800

    test: add tests to make sure core functionality works on navbar and filter dropdown

--- __tests__/FilterDropdown.test.tsx
+++ __tests__/FilterDropdown.test.tsx
@@ -2,19 +2,42 @@ import { render, screen, fireEvent } from "@testing-library/react"
 import FilterDropdown from "@/components/FilterDropdown/FilterDropdown"
 import { ReduxProvider } from "@/redux/provider"
 
-it("should change the input color when a food name filter icon is clicked", () => {
-    let testFunction: any = () => {
+describe('FilterDropdown logic', () => {
+    it("should change detect when a cuisine option is clicked and 'unclicked' in the filter dropdown", () => {
 
-    }
-    render(
-        <ReduxProvider>
-            <FilterDropdown
-                setFilterDropdownStatus={testFunction}
-            />
-        </ReduxProvider>
-    )
-    const filterDropdownFoodTypes = screen.getAllByTestId("filterdropdown__foodtype")
-    fireEvent.click(filterDropdownFoodTypes[0])
-    const filterDropdownFoodTypesActive = screen.getAllByTestId("filterdropdown__foodtype__active")
-    expect(filterDropdownFoodTypesActive[0]).toBeInTheDocument()
+        const filterDropdownStatus = "open"
+
+        render(
+            <ReduxProvider>
+                <FilterDropdown
+                    filterDropdownStatus={filterDropdownStatus}
+                    closeForm={() => { }}
+                />
+            </ReduxProvider>
+        )
+        let filterFormCuisines = screen.getAllByTestId("filter-form__cuisine")
+        const originalFormCuisinesLength = filterFormCuisines.length
+        let firstCuisine = filterFormCuisines[0]
+
+        //Make sure that the desired element is not already present
+        expect(screen.queryByTestId("filter-form__cuisine filter-form__cuisine--clicked", { exact: true }))
+            .not.toBeInTheDocument()
+        fireEvent.click(firstCuisine)
+
+        //Desired element should now be in the document
+        let filterFormCuisineClicked = screen.getByTestId("filter-form__cuisine filter-form__cuisine--clicked", { exact: true })
+        expect(filterFormCuisineClicked)
+            .toBeInTheDocument()
+
+        //There should be one less cuisine without the clicked data-testid
+        filterFormCuisines = screen.getAllByTestId("filter-form__cuisine", { exact: true })
+        expect(filterFormCuisines.length)
+            .toBe(originalFormCuisinesLength - 1)
+
+        //When the same desired element is clicked again, everything should be reset
+        fireEvent.click(firstCuisine)
+        filterFormCuisines = screen.getAllByTestId("filter-form__cuisine", { exact: true })
+        expect(filterFormCuisines.length)
+            .toBe(originalFormCuisinesLength)
+    })
 })
--- __tests__/Navbar.test.tsx
+++ __tests__/Navbar.test.tsx
@@ -2,10 +2,10 @@ import { render, screen, fireEvent } from "@testing-library/react"
 import Navbar from "@/components/Navbar/Navbar"
 import { ReduxProvider } from "@/redux/provider"
 
-describe("Navbar", () => {
+describe("Render navbar dropdowns/buttons", () => {
     it("should render the filter icon", () => {
         render(<Navbar />)
-        const filterButton = screen.getByTestId("navbar__filter__button")
+        const filterButton = screen.getByTestId("navbar-filter__button")
 
         expect(filterButton).toBeInTheDocument()
     })
@@ -16,9 +16,66 @@ describe("Navbar", () => {
                 <Navbar />
             </ReduxProvider>
         )
-        const filterButton = screen.getByTestId("navbar__filter__button")
+        const filterButton = screen.getByTestId("navbar-filter__button")
         fireEvent.click(filterButton)
-        const filterdropdown = screen.getByTestId("filterdropdown")
-        expect(filterdropdown).toBeInTheDocument()
+
+        const filterDropdown = screen.getByTestId("filter-dropdown")
+        expect(filterDropdown).toBeInTheDocument()
+    })
+
+    it("should render the account dropdown", () => {
+        render(
+            <ReduxProvider>
+                <Navbar />
+            </ReduxProvider>
+        )
+        const accountButton = screen.getByTestId("navbar-account__button")
+        fireEvent.click(accountButton)
+
+        const accountDropdown = screen.getByTestId("account-dropdown")
+        expect(accountDropdown).toBeInTheDocument()
+    })
+
+
+    it("should detect which NavbarDropdown is in the document", () => {
+        render(
+            <ReduxProvider>
+                <Navbar />
+            </ReduxProvider>
+        )
+        Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 500 })
+
+        const navbarHamburger = screen.getByTestId("navbar__hamburger")
+        let navbarDropdownClosed = screen.queryByTestId("navbar__dropdown--closed")
+        let navbarDropdown = screen.queryByTestId("navbar__dropdown")
+        expect(navbarDropdownClosed)
+            .toBeInTheDocument()
+        expect(navbarDropdown)
+            .not.toBeInTheDocument()
+
+        fireEvent.click(navbarHamburger)
+
+        navbarDropdownClosed = screen.queryByTestId("navbar__dropdown--closed")
+        navbarDropdown = screen.queryByTestId("navbar__dropdown")
+        expect(navbarDropdownClosed)
+            .not.toBeInTheDocument()
+        expect(navbarDropdown)
+            .toBeInTheDocument()
+    })
+
+    it("should close FilterDropdown on wrapper click", () => {
+        render(
+            <ReduxProvider>
+                <Navbar />
+            </ReduxProvider>
+        )
+        let filterButton = screen.getByTestId("navbar-filter__button")
+        fireEvent.click(filterButton)
+
+        let filterDropdown: HTMLElement | null = screen.getByTestId("filter-dropdown")
+        fireEvent.click(filterDropdown)
+
+        filterDropdown = screen.queryByTestId("filter-dropdown")
+        expect(filterDropdown).not.toBeInTheDocument()
     })
 })
