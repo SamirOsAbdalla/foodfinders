53827b2fae4b76bc05db65d6e655e106c8bc6f83
samir01 <samir.os.abdalla@gmail.com>
2023-12-15 19:12:35 -0800

    refactor: users can no longer toggle the instructions

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -15,24 +15,18 @@ export default function Instructions() {
     }
 
     return (
-        <div className="htu__wrapper d-flex flex-column">
-            <div className="htu__heading d-flex justify-content-center align-items-center">
-                <div className="htu__heading--container d-flex justify-content-between align-items-center">
-                    Instructions
-                    <div>
-                        {caretStatus == "down" ?
-                            <FaCaretDown onClick={toggleCaretStatus} className="htu__caret" /> :
-                            <FaCaretUp onClick={toggleCaretStatus} className="htu__caret" />
-                        }
-                    </div>
-                </div>
+        <div className="htu__wrapper d-flex flex-column gap-3">
+            <div className="htu__heading--container d-flex justify-content-between align-items-center">
+                Instructions
+
             </div>
-            <div className={`htu__dropdown pt-2 ${caretStatus == "up" && "htu__dropdown--show"} d-flex flex-column justify-content-start gap-4`}>
-                <span>1. Click on the button</span>
-                <span>2. Get your personalized restaurant</span>
-                <span>3. Call, favorite or go to the restaurant</span>
-                <span>{"4. Eat (maybe)"}</span>
-                <span>5. Repeat </span>
+            <div className={`htu__dropdown d-flex flex-column justify-content-start gap-3`}>
+                <span>1. Allow location services</span>
+                <span>2. Click on the button</span>
+                <span>3. Get your personalized restaurant</span>
+                <span>4. Call, favorite or go to the restaurant</span>
+                <span>{"5. Eat (maybe)"}</span>
+                <span>6. Repeat </span>
             </div>
         </div>
     )
