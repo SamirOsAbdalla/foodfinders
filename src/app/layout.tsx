25c79093d33e5f82b2b9a5156c1fec3223ab74f4
samir01 <samir.os.abdalla@gmail.com>
2023-12-19 14:14:06 -0800

    refactor: wrapped components in Providers component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,26 +5,31 @@ import './globals.css'
 import BootstrapClient from "@/components/BootstrapClient"
 import { ReduxProvider } from "@/redux/provider"
 import Navbar from "@/components/Navbar/Navbar"
+import Providers from "@/components/Providers"
+import { getServerSession } from "next-auth"
 
 const inter = Inter({ subsets: ['latin'] })
-
 export const metadata: Metadata = {
   title: 'SpeedEats',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const session = await getServerSession()
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ReduxProvider>
-          <Navbar />
-          {children}
-          <BootstrapClient />
-        </ReduxProvider>
+        <Providers session={session}>
+          <ReduxProvider>
+            <Navbar />
+            {children}
+            <BootstrapClient />
+          </ReduxProvider>
+        </Providers>
       </body>
     </html>
   )
