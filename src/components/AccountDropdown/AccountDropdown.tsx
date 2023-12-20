2ea36626815f32391202b4715b71d7615709958e
samir01 <samir.os.abdalla@gmail.com>
2023-12-20 13:23:21 -0800

    style: moved comment location

--- src/components/AccountDropdown/AccountDropdown.tsx
+++ src/components/AccountDropdown/AccountDropdown.tsx
@@ -22,13 +22,14 @@ export default function AccountDropdown({
         )
     }
 
-    // Practicing with SOLID principles led to me try and implement the links using the
-    // open closed principle
 
     const onClickLogout = async () => {
         closeAccountDropdown()
         await signOut()
     }
+
+    // Practicing with SOLID principles led to me try and implement the links using the
+    // open closed principle
     return (
         <div data-testid="account-dropdown" className="account-dropdown__wrapper py-2  position-absolute d-flex flex-column gap-2">
             <DropdownLink
