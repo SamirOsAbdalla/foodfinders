ee04912f2187b6e888b5df5ea62ccb3985d40746
samir01 <samir.os.abdalla@gmail.com>
2023-12-20 13:24:05 -0800

    refactor: removed unnecessary classname from body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css"
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import BootstrapClient from "@/components/BootstrapClient"
 import { ReduxProvider } from "@/redux/provider"
@@ -8,7 +7,6 @@ import Navbar from "@/components/Navbar/Navbar"
 import Providers from "@/components/Providers"
 import { getServerSession } from "next-auth"
 
-const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'SpeedEats',
 }
@@ -22,7 +20,7 @@ export default async function RootLayout({
   const session = await getServerSession()
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers session={session}>
           <ReduxProvider>
             <Navbar />
