d333cdfc990a4b4539d3358eaf5d4e400f70e92c
samir01 <samir.os.abdalla@gmail.com>
2023-12-20 13:28:49 -0800

    refactor: added favorites reducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,12 +3,15 @@ import filterReducer from "./slices/filter-slice"
 import currentRestaurantReducer from "./slices/currentRestaurant-slice"
 import restaurantHistoryReducer from "./slices/restaurantHistory-slice";
 import restaurantErrorReducer from "./slices/restaurantError-slice"
+import favoritesReducer from "./slices/favorites-slice"
+
 export const store = configureStore({
     reducer: {
         filterReducer,
         currentRestaurantReducer,
         restaurantHistoryReducer,
-        restaurantErrorReducer
+        restaurantErrorReducer,
+        favoritesReducer
     }
 })
 
