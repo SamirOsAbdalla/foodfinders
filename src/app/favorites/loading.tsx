177f2ad64f6981e0ca488ea13ec7cf909eeddcd4
samir01 <samir.os.abdalla@gmail.com>
2023-12-23 13:29:23 -0800

    refactor: added styling for loading page

--- src/app/favorites/loading.tsx
+++ src/app/favorites/loading.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function loading() {
     return (
-        <div className="pt-5">loading...</div>
+        <div className="d-flex align-items-center justify-content-center">
+            Loading...
+        </div>
     )
 }
