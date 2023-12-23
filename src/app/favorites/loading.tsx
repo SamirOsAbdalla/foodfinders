8356ebd2ef723a9051303606ca2d1676a55987f0
samir01 <samir.os.abdalla@gmail.com>
2023-12-23 13:58:48 -0800

    refactor: edited loading message

--- src/app/favorites/loading.tsx
+++ src/app/favorites/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function loading() {
     return (
         <div className="d-flex align-items-center justify-content-center">
-            Loading...
+            Loading Favorites...
         </div>
     )
 }
