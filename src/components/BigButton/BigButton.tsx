19ddd05df95f04d880e0139551798f85e36e7439
samir01 <samir.os.abdalla@gmail.com>
2023-12-24 12:56:28 -0800

    refactor: big button now handles responsibility of checking cached api response before making api call

--- src/components/BigButton/BigButton.tsx
+++ src/components/BigButton/BigButton.tsx
@@ -4,30 +4,85 @@ import { useState } from "react"
 import { IoFastFoodOutline } from "react-icons/io5";
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
-import { setCurrentRestaurant } from "@/redux/slices/currentRestaurant-slice";
+import { currentRestaurant, setCurrentRestaurant } from "@/redux/slices/currentRestaurant-slice";
 import { setRestaurantHistory } from "@/redux/slices/restaurantHistory-slice";
 import { setRestaurantError } from "@/redux/slices/restaurantError-slice";
+import { YelpRestaurant } from "@/util/restaurantTypes";
+import { popCachedRestaurant, setCachedRestaurants } from "@/redux/slices/cachedRestaurants-slice";
+import { FiltersObject } from "@/util/restaurantTypes";
+import { mapPricesToNumberString } from "@/util/getRestaurant";
+import { defaultDistanceRadius } from "@/util/constants";
+import { defaultPrevYelpFilters } from "@/redux/slices/cachedRestaurants-slice";
+
+function arePrevFiltersASubset(filtersObject: FiltersObject, prevYelpFilters: any) {
+
+    //We must make the API call since the previous filters are too general for the current call
+    if (prevYelpFilters.isDefault &&
+        (filtersObject.prices.length > 0 ||
+            filtersObject.cuisines.length > 0 ||
+            parseInt(filtersObject.filterDistance) <= parseInt(defaultDistanceRadius))) {
+
+        return false
+    }
+
+    let prevPricesString = prevYelpFilters.prevPricesString
+    let prevCuisinesString = prevYelpFilters.prevCuisinesString
+    let currentPricesString = mapPricesToNumberString(filtersObject.prices)
+    let currentCuisinesString = filtersObject.cuisines.sort().join(",").toLowerCase()
+
+    if (currentPricesString.includes(prevPricesString)
+        && currentCuisinesString.includes(prevCuisinesString)
+        && parseInt(filtersObject.filterDistance) >= parseInt(prevYelpFilters.prevDistance)) {
+        return true
+    }
+    return false
+}
 
 function useGeoLocation() {
     const reduxFilterState = useSelector((state: RootState) => state.filterReducer.value)
-    const dispatch = useDispatch<AppDispatch>()
+    const reduxCacheState = useSelector((state: RootState) => state.cachedRestaurantsReducer.value)
 
+    const dispatch = useDispatch<AppDispatch>()
     const [loading, setLoading] = useState<boolean>(false)
 
     const geolocationSuccess = async (position: GeolocationPosition) => {
         setLoading(true)
-        const coords = position.coords
+
+        const currentFiltersObject: FiltersObject = {
+            prices: reduxFilterState.prices,
+            cuisines: reduxFilterState.cuisines,
+            filterDistance: reduxFilterState.filterDistance
+        }
+
+        if (
+            reduxCacheState.cachedRestaurants.length > 0 &&
+            arePrevFiltersASubset(currentFiltersObject, reduxCacheState.prevYelpFilters)) {
+
+            if (reduxCacheState.currentApiType == "yelp") {
+                let cachedRestaurant = reduxCacheState.cachedRestaurants[reduxCacheState.cachedRestaurants.length - 1] as YelpRestaurant
+                dispatch(setCurrentRestaurant({ currentRestaurant: cachedRestaurant }))
+                dispatch(setRestaurantHistory(cachedRestaurant))
+                dispatch(popCachedRestaurant())
+            }
+            setLoading(false)
+            return
+        }
 
         const cuisineString = reduxFilterState.cuisines.join(",")
         const pricesString = reduxFilterState.prices.join(",")
         const filterDistance = reduxFilterState.filterDistance
+
+        const coords = position.coords
+
+        let initialApiToFetch = reduxCacheState.currentApiType
         const fetchUrl = `/api/getNearbyRestaurant?` +
             `latitude=${coords.latitude}&longitude=${coords.longitude}` +
             `${cuisineString ? ("&cuisineString=" + cuisineString) : ""}` +
             `${pricesString ? ("&pricesString=" + pricesString) : ""}` +
-            `${filterDistance ? ("&filterDistance=" + filterDistance) : "13"}`
+            `${filterDistance ? ("&filterDistance=" + filterDistance) : "13"}` +
+            `&initialApiToFetch=${initialApiToFetch}`
 
-        const restaurant = await fetch(fetchUrl, {
+        const restaurants = await fetch(fetchUrl, {
             method: "GET",
             headers: {
                 "Accept": "application/json",
@@ -35,10 +90,36 @@ function useGeoLocation() {
             }
         })
 
-        const restaurantJSON = await restaurant.json()
-        if (!("error" in restaurantJSON)) {
-            dispatch(setCurrentRestaurant({ currentRestaurant: restaurantJSON }))
-            dispatch(setRestaurantHistory(restaurantJSON))
+        const restaurantsJSON = await restaurants.json()
+        if (!("error" in restaurantsJSON)) {
+
+            //we got yelp results back
+            if (Array.isArray(restaurantsJSON)) {
+                let currentRestaurant = restaurantsJSON.pop()
+
+                let filterString = reduxFilterState.cuisines.sort().join(",").toLowerCase()
+                let pricesString = mapPricesToNumberString(reduxFilterState.prices)
+                const newIsDefault = (
+                    filterString == "" &&
+                    pricesString == "" &&
+                    filterDistance == defaultDistanceRadius
+                )
+
+                let prevYelpFilters = {
+                    isDefault: newIsDefault,
+                    prevCuisinesString: filterString,
+                    prevPricesString: pricesString,
+                    prevDistance: filterDistance
+                }
+
+                dispatch(setCachedRestaurants({ cachedRestaurants: restaurantsJSON, currentApiType: "yelp", prevYelpFilters }))
+                dispatch(setCurrentRestaurant({ currentRestaurant }))
+                dispatch(setRestaurantHistory(currentRestaurant))
+            } else {
+                dispatch(setCachedRestaurants({ cachedRestaurants: [], currentApiType: "yelp", prevYelpFilters: defaultPrevYelpFilters }))
+                dispatch(setCurrentRestaurant({ currentRestaurant: restaurantsJSON }))
+                dispatch(setRestaurantHistory(restaurantsJSON))
+            }
         } else {
             dispatch(setRestaurantError(true))
         }
@@ -48,6 +129,7 @@ function useGeoLocation() {
     const geolocationError = (error: GeolocationPositionError) => {
         console.log("Error: Could not fetch user coordinates")
     }
+
     const handleBigButtonClick = () => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError)
