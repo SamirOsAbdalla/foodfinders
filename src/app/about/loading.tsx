24ed6290929213feb0c54866ca84101f4c30e82c
samir01 <samir.os.abdalla@gmail.com>
2023-12-24 16:04:50 -0800

    refactor: edited loading message

--- src/app/about/loading.tsx
+++ src/app/about/loading.tsx
@@ -3,8 +3,8 @@ import styles from './page.module.css'
 
 export default function Loading() {
     return (
-        <div className = {styles.loading}>
-            Loading
+        <div className={styles.loading}>
+            Loading...
         </div>
     )
 }
